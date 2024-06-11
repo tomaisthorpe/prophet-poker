4f4e58a20c51d4a6945a522fdc8a3a72550b799e
Tom Aisthorpe <tomaisthorpe@gmail.com>
2024-06-11 23:57:07 +0100

    fix: ensure pages are not generated statically
    This ensures the STUN/TURN config is read at runtime

--- app/host/page.tsx
+++ app/host/page.tsx
@@ -1,6 +1,11 @@
 import HostSessionPage from "@/container/HostSessionPage";
+import { unstable_noStore as noStore } from "next/cache";
 
 export default function Host() {
+  // Ensure this page is treated as dynamic
+  // So that the envvar is read at runtime
+  noStore();
+
   const peerConfig = process.env.RTC_CONFIG
     ? JSON.parse(process.env.RTC_CONFIG)
     : undefined;
--- app/play/[id]/page.tsx
+++ app/play/[id]/page.tsx
@@ -1,6 +1,11 @@
 import PlayerSessionPage from "@/container/PlayerSessionPage";
+import { unstable_noStore as noStore } from "next/cache";
 
 export default function PlaySession({ params }: { params: { id: string } }) {
+  // Ensure this page is treated as dynamic
+  // So that the envvar is read at runtime
+  noStore();
+
   const peerConfig = process.env.RTC_CONFIG
     ? JSON.parse(process.env.RTC_CONFIG)
     : undefined;
